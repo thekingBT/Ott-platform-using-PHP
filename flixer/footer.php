12771a8c5e0cddb6063b925d6e6a5dfe05827d01
bhavya trivedi <139704485+thekingBT@users.noreply.github.com>
2023-11-06 12:19:50 +0530

    Add files via upload

---
+++ flixer/footer.php
@@ -0,0 +1,8 @@
+<footer>
+	<div class="row">
+		<div class="col-lg-12">
+			
+			<p>Made by <a href="https://www.instagram.com/invites/contact/?i=fv23cgmkh14f&utm_content=31aspap" rel="nofollow">Bhadresh</a>. <a href="#" target="_blank">Get support</a>.</p>
+		</div>
+	</div>
+</footer>
